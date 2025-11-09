75fd8c0d428a7e40b37f3575cf7e1422e95ae229
mi-cawber <106614898+mi-cawber@users.noreply.github.com>
2025-11-09 08:51:58 -0500

    .

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 # generate board
 def board():
-    board = []
+    board = {}
     letters = ['a','b','c','d','e','f','g','h']
     for letter in letters:
         num = 1
         while num < 9:
-            position = f'{letter}' + f'{dig}'
-            cols.append([position, None])
+            position = f'{letter}' + f'{num}'
+            board[position] = None
             num += 1
     return board
 
